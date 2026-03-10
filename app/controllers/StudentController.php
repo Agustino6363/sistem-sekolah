84659d62eb7a19542d48295e6e1332e2c34c0691
Agustino6363 <selvinagustino248@gmail.com>
2026-03-10 13:13:06 +0700

    wip: implement student index page

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -6,13 +6,13 @@ class StudentController
     public function index()
 
     {
-        require_once './app/views/students/index.php';
+        require_once '../app/views/students/index.php';
     }
 
     public function create()
 
     {
-        require_once './app/views/students/create.php';
+        require_once '../app/views/students/create.php';
     }
 }
 ?>
